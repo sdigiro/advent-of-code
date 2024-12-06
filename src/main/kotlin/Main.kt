9be3832eb533e06298e7c26f27a359f2304883b5
Sofia <sofiadigirolamo514@gmail.com>
2024-12-06 11:10:05 -0500

    advent of code day 2 (solved at work, moved to personal repo)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,35 @@
 package org.example
 
 import days.Day1
+import org.example.days.Day2
 import java.io.File
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     // DAY 1
-    val day1 = Day1()
-
+//    val day1 = Day1()
+//
 //    val exampleListA = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\exampleListA.txt")
 //    val exampleListB = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\exampleListB.txt")
-
+//
 //    println("Day 1 Part 1 Example Answer: " + day1.day1part1(exampleListA, exampleListB))
 //    println("Day 1 Part 2 Example Answer: " + day1.day1part2(exampleListA, exampleListB))
-
-    val listA = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listA.txt")
-    val listB = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listB.txt")
-
+//
+//    val listA = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listA.txt")
+//    val listB = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listB.txt")
+//
 //    println("Day 1 Part 1 Answer: " + day1.day1part1(listA, listB))
-    println("Day 1 Part 2 Answer: " + day1.day1part2(listA, listB))
+//    println("Day 1 Part 2 Answer: " + day1.day1part2(listA, listB))
+
+    // DAY 2
+    val day2 = Day2()
+
+    val exampleDataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\exampleDataset.txt")
+    println("Day 2 Part 1 Example Answer: " + day2.day2part1(exampleDataset))
+
+    val dataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\dataset.txt")
+    println("Day 2 Part 1 Answer: " + day2.day2part1(dataset))
 }
 
 fun readFileDay1(filePath: String): MutableList<Int> {
@@ -30,4 +40,20 @@ fun readFileDay1(filePath: String): MutableList<Int> {
     }
 
     return result
+}
+
+fun readFileDay2(filePath: String): MutableList<MutableList<Int>> {
+    val data = mutableListOf<MutableList<Int>>()
+
+    File(filePath).bufferedReader().forEachLine { line ->
+        val report = mutableListOf<Int>()
+
+        line.split(" ").forEach { level ->
+            report.add(level.toInt())
+        }
+
+        data.add(report)
+    }
+
+    return data
 }
--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -1,4 +1,98 @@
 package org.example.days
 
+import kotlin.math.absoluteValue
+
 class Day2 {
+    // Day 2 Part 1
+    // the data consists of many reports, one per line
+    // each report is a list of numbers called levels that are separated by spaces
+    // example containing 6 reports, each with 5 levels:
+    //   7 6 4 2 1 // safe (levels all decreasing by 1 or 2)
+    //   1 2 7 8 9 // unsafe (2 -> 7 is an increase of 5)
+    //   9 7 6 2 1 // unsafe (6 -> 2 is a decrease of 4)
+    //   1 3 2 4 5 // unsafe (1 -> 3 is an increase of 2 which is safe, but 3 -> is then decreasing)
+    //   8 6 4 4 1 // unsafe (4 -> 4 is stagnant)
+    //   1 3 6 7 9 // safe (levels all increasing by 1, 2, or 3)
+
+    // given a dataset similar to the above example, determine how many reports are safe
+    // a report is safe iff the levels are all decreasing or all increasing AND any two adjacent levels differ by at least 1 and at most 3
+    // see above for safe/unsafe reports in the example dataset
+    fun day2part1(dataset: List<List<Int>>): Int {
+        var safeCount = 0
+
+        dataset.forEach { report ->
+            val isAllInc = findIfAllIncreasing(report)
+            val isAllDec = findIfAllDecreasing(report)
+            val isSafeDiff = findIfSafeDiff(report)
+
+            if ((isAllInc || isAllDec) && isSafeDiff) {
+                safeCount++
+            }
+        }
+
+        return safeCount
+    }
+
+    private fun findIfSafeDiff(report: List<Int>): Boolean {
+        var isSafe = false
+        var diff: Int
+
+        if (findIfAllIncreasing(report) || findIfAllDecreasing(report)) {
+            report.forEach { level ->
+                if (report.indexOf(level) < report.size - 1) {
+                    if (level == report[report.indexOf(level) + 1]) {
+                        return false
+                    } else {
+                        diff = (level - report[report.indexOf(level) + 1]).absoluteValue
+
+                        if (diff in 1..3) {
+                            isSafe = true
+                        } else {
+                            return false
+                        }
+                    }
+                } else {
+                    return isSafe
+                }
+            }
+        }
+
+        return isSafe
+    }
+
+    private fun findIfAllIncreasing(report: List<Int>): Boolean {
+        var isInc = false
+
+        report.forEach { level ->
+            if (report.indexOf(level) < report.size - 1) {
+                if (level < report[report.indexOf(level) + 1]) {
+                    isInc = true
+                } else {
+                    return false
+                }
+            } else {
+                return isInc
+            }
+        }
+
+        return false
+    }
+
+    private fun findIfAllDecreasing(report: List<Int>): Boolean {
+        var isDec = false
+
+        report.forEach { level ->
+            if (report.indexOf(level) < report.size - 1) {
+                if (level > report[report.indexOf(level) + 1]) {
+                    isDec = true
+                } else {
+                    return false
+                }
+            } else {
+                return isDec
+            }
+        }
+
+        return false
+    }
 }
