0ca6385d74244ea08eb8689fe1b6b78cea71a6b1
Sofia <sofiadigirolamo514@gmail.com>
2024-12-06 11:38:10 -0500

    advent of code day 3 part 1 (solved at work, moved to personal repo)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package org.example
 
 import days.Day1
 import org.example.days.Day2
+import org.example.days.Day3
 import java.io.File
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -23,13 +24,22 @@ fun main() {
 //    println("Day 1 Part 2 Answer: " + day1.day1part2(listA, listB))
 
     // DAY 2
-    val day2 = Day2()
+//    val day2 = Day2()
+//
+//    val exampleDataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\exampleDataset.txt")
+//    println("Day 2 Part 1 Example Answer: " + day2.day2part1(exampleDataset))
+//
+//    val dataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\dataset.txt")
+//    println("Day 2 Part 1 Answer: " + day2.day2part1(dataset))
 
-    val exampleDataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\exampleDataset.txt")
-    println("Day 2 Part 1 Example Answer: " + day2.day2part1(exampleDataset))
+    // DAY 3
+    val day3= Day3()
 
-    val dataset = readFileDay2("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day2\\dataset.txt")
-    println("Day 2 Part 1 Answer: " + day2.day2part1(dataset))
+    val exampleMemory = readFileDay3("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day3\\exampleMemory.txt")
+    println("Day 3 Part 1 Example Answer: " + day3.day3part1(exampleMemory))
+
+    val memory = readFileDay3("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day3\\memory.txt")
+    println("Day 3 Part 1 Answer: " + day3.day3part1(memory))
 }
 
 fun readFileDay1(filePath: String): MutableList<Int> {
@@ -56,4 +66,8 @@ fun readFileDay2(filePath: String): MutableList<MutableList<Int>> {
     }
 
     return data
+}
+
+fun readFileDay3(filePath: String): String {
+    return File(filePath).inputStream().readBytes().toString(Charsets.UTF_8)
 }
--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -1,4 +1,37 @@
 package org.example.days
 
+import java.util.regex.Pattern
+
 class Day3 {
+    fun day3part1(memory: String): Int {
+        var result = 0
+        val matchList = mutableListOf<String>()
+
+        val mulRegex = Regex("mul\\([0-9]+,[0-9]+\\)")
+        val mulRegexPattern = Pattern.compile(mulRegex.pattern)
+        val mulRegexMatcher = mulRegexPattern.matcher(memory)
+
+        while (mulRegexMatcher.find()) {
+            matchList.add(mulRegexMatcher.group())
+        }
+
+        matchList.forEach { match ->
+            val numRegex = Regex("[0-9]+")
+            val numRegexPattern = Pattern.compile(numRegex.pattern)
+            val numRegexMatcher = numRegexPattern.matcher(match)
+
+            val numPair = mutableListOf<String>()
+            while (numRegexMatcher.find()) {
+                numPair.add(numRegexMatcher.group())
+            }
+
+            result += mul(numPair[0].toInt(), numPair[1].toInt())
+        }
+
+        return result
+    }
+
+    private fun mul(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
 }
