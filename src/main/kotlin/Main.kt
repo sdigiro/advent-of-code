f2dcc60315020f2145deab6a919b3e4b710e44ef
Sofia <sofiadigirolamo514@gmail.com>
2024-12-06 10:34:42 -0500

    advent of code day 1 (solved at work, moved to personal repo)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 package org.example
 
+import days.Day1
+import java.io.File
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // DAY 1
+    val day1 = Day1()
 
+//    val exampleListA = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\exampleListA.txt")
+//    val exampleListB = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\exampleListB.txt")
+
+//    println("Day 1 Part 1 Example Answer: " + day1.day1part1(exampleListA, exampleListB))
+//    println("Day 1 Part 2 Example Answer: " + day1.day1part2(exampleListA, exampleListB))
+
+    val listA = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listA.txt")
+    val listB = readFileDay1("C:\\Users\\Sofia\\projects\\advent-of-code\\src\\main\\resources\\Day1\\listB.txt")
+
+//    println("Day 1 Part 1 Answer: " + day1.day1part1(listA, listB))
+    println("Day 1 Part 2 Answer: " + day1.day1part2(listA, listB))
+}
+
+fun readFileDay1(filePath: String): MutableList<Int> {
+    val result = mutableListOf<Int>()
+
+    File(filePath).bufferedReader().forEachLine { line ->
+        result.add(line.toInt())
+    }
+
+    return result
 }
--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,5 +1,8 @@
 package days
 
+import java.util.LinkedList
+import kotlin.math.absoluteValue
+
 class Day1 {
     // Day 1 Part 1
     // given two lists, pair the numbers from lowest to highest, calculate their differences, and sum the differences
@@ -8,6 +11,71 @@ class Day1 {
     //   differences are: [3 - 2 = 1], [4 - 3 = 1], and [5 - 4 = 1]
     //   sum of differences is: 1 + 1 + 1 = 3
     fun day1part1(listA: MutableList<Int>, listB: MutableList<Int>): Int {
-        return 0
+        val matchedPairs = LinkedList<Pair<Int, Int>>()
+        val pairDiffs = mutableListOf<Int>()
+
+        while (listA.isNotEmpty() && listB.isNotEmpty()) {
+            var lowestListA = listA[0]
+            var lowestListB = listB[0]
+
+            var lowestListAIndex = listA.indexOf(lowestListA)
+            var lowestListBIndex = listB.indexOf(lowestListB)
+
+            listA.forEach { valueA ->
+                if(valueA < lowestListA) {
+                    lowestListA = valueA
+                    lowestListAIndex = listA.indexOf(valueA)
+                }
+            }
+
+            listB.forEach { valueB ->
+                if (valueB < lowestListB) {
+                    lowestListB = valueB
+                    lowestListBIndex = listB.indexOf(valueB)
+                }
+            }
+
+            matchedPairs.add(Pair(lowestListA, lowestListB))
+
+            listA.removeAt(lowestListAIndex)
+            listB.removeAt(lowestListBIndex)
+        }
+
+        matchedPairs.forEach { pair ->
+            val diff = (pair.first - pair.second).absoluteValue
+            pairDiffs.add(diff)
+        }
+
+        var result = 0
+        pairDiffs.forEach { diff ->
+            result += diff
+        }
+
+        return result
+    }
+
+    // Day 1 Part 2
+    // given two lists, determine how often each number in listA appears in listB, then determine the similarity score
+    // similarity score = sum of (listA value * how often it appears in listB)
+    fun day1part2(listA: MutableList<Int>, listB: MutableList<Int>): Int {
+        val counts: MutableList<Pair<Int, Int>> = mutableListOf()
+        var similarityScore = 0
+
+        listA.forEach { valueA ->
+            var count = 0
+            listB.forEach { valueB ->
+                if (valueB == valueA) {
+                    count++
+                }
+            }
+
+            counts.add(Pair(valueA, count))
+        }
+
+        counts.forEach { pair ->
+            similarityScore += pair.first * pair.second
+        }
+
+        return similarityScore
     }
 }
